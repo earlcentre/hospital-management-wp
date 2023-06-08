51b2f4ed649d211e8fa810bac1c0da530043a41c
musangi <faithmusangi50@gmail.com>
2023-06-08 10:37:18 +0300

    modifying plugin description

--- hospital-management.php
+++ hospital-management.php
@@ -1,16 +1,15 @@
 <?php
 /*
 Plugin Name: Hospital Management
-Plugin URI: http://www.mobilewebs.net/mojoomla/extend/wordpress/hospital/
 Description: Hospital Management System for wordpress plugin is ideal way to manage complete hospital operation. It has different user roles like doctor, patient, nurse, pharmacist , accountant, laboratory staff and Support staff.
 Version: 43.0(18-06-2022)
-Author: Mojoomla
-Author URI: http://codecanyon.net/user/dasinfomedia
+Author: EarlCentre Team
+Author URI: http://earlcentre.com
 Text Domain: hospital_mgt
 Domain Path: /languages/
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Copyright 2015  Mojoomla  (email : sales@mojoomla.com)
+Copyright 2020  Earl Centre  (email : sales@earlcentre.com)
 */
 define( 'HMS_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 define( 'HMS_PLUGIN_DIR', untrailingslashit( dirname( __FILE__ ) ) );
